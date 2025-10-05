b453f2c1f3c031ed4477755ec8631eb07b6904f1
karthikgarikina <155700123+karthikgarikina@users.noreply.github.com>
2025-10-05 11:44:02 +0530

    Time: 45 ms (14.29%) | Memory: 34.4 MB (28.57%) - LeetSync

--- 4019-remove-k-balanced-substrings/remove-k-balanced-substrings.cpp
+++ 4019-remove-k-balanced-substrings/remove-k-balanced-substrings.cpp
@@ -4,46 +4,46 @@ public:
         int left=0,right=0;
         string ans="";
         string s=str;
-        int c=2;
-        while(c--){
-        int n=s.size(),idx=-1;
-        for(int i=0;i<n;i++){
-            if(s[i]=='(') break;
-            ans+=')';
-            idx=i;
-        }
-        for(int i=idx+1;i<n;i++){
-            if(s[i]=='('){
-                if(right!=0){
-                    while(left>0){
-                        ans+='(';
-                        left--;
+        int limit=2;
+        while(limit--){
+            int n=s.size(),idx=-1;
+            for(int i=0;i<n;i++){
+                if(s[i]=='(') break;
+                ans+=')';
+                idx=i;
+            }
+            for(int i=idx+1;i<n;i++){
+                if(s[i]=='('){
+                    if(right!=0){
+                        while(left>0){
+                            ans+='(';
+                            left--;
+                        }
+                        while(right>0){
+                            ans+=')';
+                            right--;
+                        }
                     }
-                    while(right>0){
-                        ans+=')';
-                        right--;
+                    left++;
+                }
+                else{
+                    right++;
+                    if(min(left,right)==k){
+                        left-=k;
+                        right-=k;
                     }
                 }
-                left++;
             }
-            else{
-                right++;
-                if(min(left,right)==k){
-                    left-=k;
-                    right-=k;
-                }
+            while(left>0){
+                ans+='(';
+                left--;
             }
-        }
-        while(left>0){
-            ans+='(';
-            left--;
-        }
-        while(right>0){
-            ans+=')';
-            right--;
-        }
-        s=ans;
-        ans="";
+            while(right>0){
+                ans+=')';
+                right--;
+            }
+            s=ans;
+            ans="";
         }
         return s;
     }
