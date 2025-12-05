e28f362390a59a37242d1209d9bbd61cc4ea5e3e
karthikgarikina <155700123+karthikgarikina@users.noreply.github.com>
2025-12-05 11:17:13 +0530

    Time: 147 ms (30.56%) | Memory: 145.7 MB (26.54%) - LeetSync

---
+++ 2388-replace-elements-in-an-array/replace-elements-in-an-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& operations) {
+        unordered_map<int,int>Map;
+        vector<int>ans=nums;
+        int n=nums.size();
+        for(int i=0;i<n;i++) Map[nums[i]]=i;
+        for(auto i : operations){
+            int a=i[0],b=i[1];
+            ans[Map[a]]=b;
+            Map[b]=Map[a];
+            // Map.erase(Map[i[0]]);
+        };
+        return ans;
+    }
+};
