4a2f64a7205fe2025e416df0943598c5d18e3501
karthikgarikina <155700123+karthikgarikina@users.noreply.github.com>
2025-12-10 09:46:00 +0530

    Time: 4 ms (36.00%) | Memory: 92.7 MB (71.00%) - LeetSync

--- 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
-    int factorial(int n){
+    int countPermutations(vector<int>& complexity) {
         long long mod=1e9+7,ans=1;
-        for(int i=1;i<=n;i++){
+        int n=complexity.size();
+        for(int i=1;i<n;i++){
+            if(complexity[0]>=complexity[i]) return 0;
             ans=(ans*i)%mod;
         }
         return ans;
-    }
-    int countPermutations(vector<int>& complexity) {
-        int num=complexity[0];
-        sort(complexity.begin(),complexity.end());
-        if(complexity[0]!=num || complexity[0]==complexity[1]){
-            return 0;
-        }
-        else return factorial(complexity.size()-1);
     }
 };
