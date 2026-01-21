ba789185a601be25e8d749639b5f34eacd4b5356
karthikgarikina <155700123+karthikgarikina@users.noreply.github.com>
2026-01-21 15:09:19 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (83.50%) - LeetSync

---
+++ 3611-construct-the-minimum-bitwise-array-ii/construct-the-minimum-bitwise-array-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int>ans;
+        for(auto i : nums){
+            int firstUnset=(~i) & (i + 1);
+            if(firstUnset==1) ans.push_back(-1);
+            else if(firstUnset>i) ans.push_back((firstUnset>>1)-1);
+            else ans.push_back(i-(firstUnset>>1));
+        }
+        return ans;
+          
+    }
+};
