9677fc87d65bb3c6f55bc9148848324745e640e9
karthikgarikina <155700123+karthikgarikina@users.noreply.github.com>
2026-01-25 18:24:38 +0530

    Time: 866 ms (15.40%) | Memory: 497.2 MB (9.55%) - LeetSync

--- 4180-pythagorean-distance-nodes-in-a-tree/pythagorean-distance-nodes-in-a-tree.cpp
+++ 4180-pythagorean-distance-nodes-in-a-tree/pythagorean-distance-nodes-in-a-tree.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    void dfs(int node, vector<vector<int>>&adj, int dis, vector<vector<long long>>&dist, int idx, vector<bool>&visited){
-        visited[node]=true;
+    void dfs(int node, vector<vector<int>>&adj, long long dis, vector<vector<long long>>&dist, int idx, vector<char>&visited){
+        visited[node]=1;
         dist[node][idx]=dis;
         for(auto i : adj[node]){
             if(!visited[i]) dfs(i,adj,dis+1,dist,idx,visited);
@@ -10,6 +10,8 @@ public:
     bool find(vector<long long>&arr){
         return (((arr[0]*arr[0])+(arr[1]*arr[1]))==(arr[2]*arr[2]));
     }
+
+
     int specialNodes(int n, vector<vector<int>>& edges, int x, int y, int z) {
         vector<vector<int>>adj(n);
         for(auto i : edges){
@@ -18,11 +20,12 @@ public:
             adj[v].push_back(u);
         }
         vector<vector<long long>>dist(n,vector<long long>(3));
-        vector<bool>visited(n,false);
+        vector<char>visited(n,0);
+        
         dfs(x,adj,0,dist,0,visited);
-        fill(visited.begin(), visited.end(), false);
+        fill(visited.begin(), visited.end(), 0);
         dfs(y,adj,0,dist,1,visited);
-        fill(visited.begin(), visited.end(), false);
+        fill(visited.begin(), visited.end(), 0);
         dfs(z,adj,0,dist,2,visited);
 
         int ans=0;
