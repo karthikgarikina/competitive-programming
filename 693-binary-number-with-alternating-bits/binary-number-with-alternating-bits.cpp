7f6813ae90afcd4126eca32c5e4baeee10d487f4
karthikgarikina <155700123+karthikgarikina@users.noreply.github.com>
2026-02-18 13:59:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (30.66%) - LeetSync

--- 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     bool hasAlternatingBits(int n) {
-        long long s=log2(n);
-        long long total=(long long)(1<<s)+(1<<s)-1;
-        return ((n^(n>>1))==total);
+        return (long long)((n^(n>>1))==(long long)(1<<(long long)log2(n))+(1<<(long long)log2(n))-1);
     }
 };
