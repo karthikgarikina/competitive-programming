03629a047048f30cea1570a98195d568f44ed8e0
karthikgarikina <155700123+karthikgarikina@users.noreply.github.com>
2026-02-28 16:07:32 +0530

    Time: 41 ms (61.75%) | Memory: 8.4 MB (17.45%) - LeetSync

---
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+        long long ans=0,mod=1e9+7;
+        for(int i=1;i<=n;i++){
+            int len=log2(i)+1;
+            ans=((ans<<len)|i)%mod;
+        }
+        return ans%mod;
+    }
+};
