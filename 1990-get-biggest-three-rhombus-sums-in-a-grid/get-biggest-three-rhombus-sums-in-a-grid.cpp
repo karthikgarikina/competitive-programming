0f81bb2c43c7a15d057987b6775ea29d82418f16
karthikgarikina <155700123+karthikgarikina@users.noreply.github.com>
2026-03-16 21:29:26 +0530

    Time: 242 ms (11.03%) | Memory: 69.1 MB (6.21%) - LeetSync

---
+++ 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int findsum(vector<vector<int>>&grid, int i, int j, int len){
+        int sum=0;
+        int temp=len;
+        while(temp--){
+            sum+=grid[i][j];
+            i++,j--;
+        }
+        temp=len;
+        while(temp--){
+            sum+=grid[i][j];
+            i++;
+            j++;
+        }
+        temp=len;
+        while(temp--){
+            sum+=grid[i][j];
+            i--;
+            j++;
+        }
+        temp=len;
+        while(temp--){
+            sum+=grid[i][j];
+            i--;
+            j--;
+        }
+
+        return sum;
+    }
+    vector<int> getBiggestThree(vector<vector<int>>& grid) {
+        vector<vector<int>>dig1,dig2;
+        unordered_map<int,bool>Map;
+        int m=grid.size(),n=grid[0].size();
+        set<int>st;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                st.insert(grid[i][j]);
+                Map[grid[i][j]]=true;
+                int len=1;
+                while((j-len)>=0 and (j+len)<n and (i+(2*len))<m){
+                    int sum=findsum(grid,i,j,len);
+                    st.insert(sum);
+                    len++;
+                }
+            }
+        }
+        vector<int>ans;
+        int size=st.size();
+        int k=3;
+        for(auto it = st.rbegin(); it != st.rend() && k--; it++){
+            ans.push_back(*it);
+        }
+        return ans;
+    }
+};
