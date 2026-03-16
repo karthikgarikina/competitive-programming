f2c9378a49182c88e862f8420c1c6299593f2d75
karthikgarikina <155700123+karthikgarikina@users.noreply.github.com>
2026-03-16 15:57:30 +0530

    Time: 160 ms (66.67%) | Memory: 254.6 MB (86.67%) - LeetSync

--- 4253-minimum-cost-to-equalize-arrays-using-swaps/minimum-cost-to-equalize-arrays-using-swaps.cpp
+++ 4253-minimum-cost-to-equalize-arrays-using-swaps/minimum-cost-to-equalize-arrays-using-swaps.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
     int minCost(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int>Map1,Map2,extra;
-        for(auto i : nums1) Map1[i]++;
-        for(auto i : nums2) Map2[i]++;
+        int n=nums1.size();
+        unordered_map<int,int>Map;
+        for(int i=0;i<n;i++){
+            Map[nums1[i]]++;
+            Map[nums2[i]]--;
+        }
+        int ans1=0,ans2=0;
 
-        int ans=0,cnt=0,temp1=0,temp2=0;
-        for(auto i : Map1){
-            int val=i.first,freq1=i.second;
-            if(Map2.find(val)==Map2.end()){
-                if(freq1%2!=0) return -1;
-                temp1+=freq1;
-                continue;
-            }
-            int freq2=Map2[val];
-            extra[val]++;
-            if((freq2+freq1)%2!=0) return -1;
-            int mii=min(freq1,freq2),mxx=max(freq1,freq2);
-            if(freq1==mxx){
-                temp1+=(mxx-mii);
-            }
-            else temp2+=(mxx-mii);
+        for(auto i : Map){
+            int freq=i.second;
+            if(freq%2!=0) return -1;
+
+            if(freq>0) ans1+=(freq/2);
+            else ans2+=((-1*freq)/2);
         }
-        for(auto i : Map2){
-            if(extra.find(i.first)!=extra.end()) continue;
-            if(i.second%2!=0) return -1;
-            temp2+=i.second;
-        }
-        int mini=min(temp1,temp2);
-        ans+=(mini/2);
-        ans+=((temp1-mini)/2);
-        ans+=((temp2-mini)/2);
-        return ans;
+        return min(ans1,ans2);
     }
 };
