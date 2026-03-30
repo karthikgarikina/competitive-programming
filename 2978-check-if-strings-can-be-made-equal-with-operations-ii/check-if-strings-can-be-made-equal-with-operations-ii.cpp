c24f18928bd6f5ee360a73e63eba1509c135e480
karthikgarikina <155700123+karthikgarikina@users.noreply.github.com>
2026-03-30 21:26:46 +0530

    Time: 2 ms (95.40%) | Memory: 19.9 MB (58.05%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -2,21 +2,17 @@ class Solution {
 public:
     bool checkStrings(string s1, string s2) {
         int n=s1.size();
-        string s1even="",s1odd="",s2even="",s2odd="";
+        vector<int>s1even(26,0),s2even(26,0),s1odd(26,0),s2odd(26,0);
         for(int i=0;i<n;i++){
             if(i%2==0){
-                s1even+=s1[i];
-                s2even+=s2[i];
+                s1even[s1[i]-'a']++;
+                s2even[s2[i]-'a']++;
             }
             else{
-                s1odd+=s1[i];
-                s2odd+=s2[i];
+                s1odd[s1[i]-'a']++;
+                s2odd[s2[i]-'a']++;
             }
         }
-        sort(s1even.begin(),s1even.end());
-        sort(s2even.begin(),s2even.end());
-        sort(s1odd.begin(),s1odd.end());
-        sort(s2odd.begin(),s2odd.end());
         return (s1even==s2even and s1odd==s2odd);
     }
 };
