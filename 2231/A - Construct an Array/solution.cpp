ee6854c67ad60d72135d0098910a6381bc16119c
karthikgarikina <155700123+karthikgarikina@users.noreply.github.com>
2026-05-21 20:16:03 +0530

    Add Construct an Array [A] from Codeforces

---
+++ 2231/A - Construct an Array/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin>>n;
+        for(int i=1;i<=n*2;i+=2) cout<<i<<" ";
+        cout<<"
+";
+    }
+ 
+    return 0;
+}
