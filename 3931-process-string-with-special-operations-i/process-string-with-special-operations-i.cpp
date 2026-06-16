fa26ad13d043f9b67c788655cbbc9d369283c75c
karthikgarikina <155700123+karthikgarikina@users.noreply.github.com>
2026-06-16 23:20:45 +0530

    Time: 5 ms (45.83%) | Memory: 39.7 MB (69.70%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string ans="";
+        for(auto i : s){
+            if(i=='*'){
+                if(!ans.empty()) ans.pop_back();
+            }
+            else if(i=='#') ans+=ans;
+            else if(i=='%'){
+                reverse(ans.begin(),ans.end());
+            }
+            else ans+=i;
+        }
+        return ans;
+    }
+};
