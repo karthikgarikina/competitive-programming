7b9ccba6b20aef83755f652ccf904cdbb672af51
karthikgarikina <155700123+karthikgarikina@users.noreply.github.com>
2026-06-20 12:46:42 +0530

    Time: 95 ms (16.67%) | Memory: 108.8 MB (11.11%) - LeetSync

---
+++ 1968-maximum-building-height/maximum-building-height.cpp
@@ -0,0 +1,84 @@
+class Solution {
+public:
+    int maxBuilding(int n, vector<vector<int>>& rest) {
+        rest.push_back({1,0});
+        int len = rest.size();
+        sort(rest.begin(), rest.end());
+        vector<int>maxis;
+        for(int i = 1; i < len; i++){
+            int prev = rest[i-1][0], prevLim = rest[i-1][1], curnt = rest[i][0], curntLim = rest[i][1];
+            if(prevLim <= curntLim){
+                int dif = curnt-prev;
+                int canMax = prevLim+dif;
+                // cout<<curnt<<" : "<<canMax<<"\n";
+                if(canMax > curntLim){
+                    canMax=(canMax+curntLim)/2;
+                    // canMax -= abs(canMax-curntLim+2)/3;
+                }
+                else{
+                    rest[i][1] = canMax;
+                }
+                maxis.push_back(canMax);
+                // cout<<curnt<<" : "<<canMax<<"\n";
+            }
+            else{
+                int dif = curnt-prev;
+                int canMax = prevLim+dif;
+                // cout<<curnt<<" : "<<canMax<<"\n";
+                if(prevLim-dif <= curntLim){
+                    canMax=(canMax+curntLim)/2;
+                    // canMax -= abs(canMax-curntLim+2)/3;
+                    maxis.push_back(canMax);
+                    // cout<<curnt<<" : "<<canMax<<"\n";
+                }
+                else{
+                    int ed=i-2;
+                    if(rest[ed][0] == -1) ed = rest[ed][1];
+                    while(ed>=0){
+                        int last = rest[ed][0], lastLim = rest[ed][1];
+                        maxis.pop_back();
+                        if(lastLim <= curntLim){
+                            int dif = curnt-last;
+                            int canMax = lastLim+dif;
+                            if(canMax > curntLim){
+                                // canMax -= abs(canMax-curntLim+2)/3;
+                                canMax=(canMax+curntLim)/2;
+                            }
+                            else{
+                                rest[i][1] = canMax;
+                            }
+                            maxis.push_back(canMax);
+                            // cout<<curnt<<" : "<<canMax<<"\n";
+                            rest[i-1][0] = -1, rest[i-1][1] = ed;
+                            break;
+                        }
+                        else{
+                            int dif = curnt-last;
+                            int canMax = lastLim+dif;
+                            if(lastLim-dif <= curntLim){
+                                // canMax -= abs(canMax-curntLim+2)/3;
+                                canMax=(canMax+curntLim)/2;
+                                maxis.push_back(canMax);
+                                // cout<<curnt<<" : "<<canMax<<"\n";
+                                rest[i-1][0] = -1, rest[i-1][1] = ed;
+                                break;
+                            }
+                            else{
+                                ed--;
+                                if(rest[ed][0]==-1) ed=rest[ed][1];
+                            }
+                        }
+
+                    }
+                }
+
+            }
+        }
+        maxis.push_back(rest.back()[1]+(n-rest.back()[0]));
+        int ans=0;
+        for(auto i : maxis){
+            ans = max(i,ans);
+        }
+        return ans;
+    }
+};
