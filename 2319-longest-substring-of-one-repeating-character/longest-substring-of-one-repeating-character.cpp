e9b24227c324d71823d0d441c50539147ff8e13a
karthikgarikina <155700123+karthikgarikina@users.noreply.github.com>
2026-08-13 20:38:58 +0530

    Time: 670 ms (9.40%) | Memory: 215.1 MB (7.38%) - LeetSync

---
+++ 2319-longest-substring-of-one-repeating-character/longest-substring-of-one-repeating-character.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    void buildTree(int idx, int l, int r, string& s, vector<vector<int>>& segTree){
+        if(l == r){
+            segTree[idx] = {1, 1, 1};
+            return;
+        }
+        int mid = (r + l) / 2;
+        int leftIdx = idx * 2 + 1;
+        int rightIdx = idx * 2 + 2;
+        buildTree(leftIdx, l, mid, s, segTree);
+        buildTree(rightIdx, mid + 1, r, s, segTree);
+        int maxi = 1, left = segTree[leftIdx][0], right = segTree[rightIdx][1];
+        if(s[mid] == s[mid + 1]){
+            maxi = segTree[leftIdx][1] + segTree[rightIdx][0];
+            if((mid - l + 1) == segTree[leftIdx][1]) left = maxi;
+            if((r - mid) == segTree[rightIdx][0]) right = maxi;
+        }
+        maxi = max({maxi, segTree[leftIdx][2], segTree[rightIdx][2]});
+        segTree[idx] = {left, right, maxi};
+    }
+
+    void query(int idx, int l, int r, int t, string& s, vector<vector<int>>& segTree){
+        if(l == t and r == t){
+            segTree[idx] = {1, 1, 1};
+            return;
+        }
+        int mid = (r + l) / 2;
+        int leftIdx = idx * 2 + 1;
+        int rightIdx = idx * 2 + 2;
+        if(t >= l and t <= mid){
+            query(leftIdx, l, mid, t, s, segTree);
+        }
+        if(t >= mid + 1 and t <= r){
+            query(rightIdx, mid + 1, r, t, s, segTree);
+        }
+        int maxi = 1, left = segTree[leftIdx][0], right = segTree[rightIdx][1];
+        if(s[mid] == s[mid + 1]){
+            maxi = segTree[leftIdx][1] + segTree[rightIdx][0];
+            if((mid - l + 1) == segTree[leftIdx][1]) left = maxi;
+            if((r - mid) == segTree[rightIdx][0]) right = maxi;
+        }
+        maxi = max({maxi, segTree[leftIdx][2], segTree[rightIdx][2]});
+        segTree[idx] = {left, right, maxi};
+    }
+
+    vector<int> longestRepeating(string s, string qc, vector<int>& qi) {
+        int n = s.size();
+        vector<vector<int>>segTree(n*4);
+        buildTree(0, 0, n - 1, s, segTree);
+        int q = qi.size();
+        vector<int>ans;
+        cout<<segTree[0][2];
+        for(int i = 0; i < q; i++){
+            int idx = qi[i];
+            char ch = qc[i];
+            s[idx] = ch;
+            query(0, 0, n - 1, idx, s, segTree);
+            ans.push_back(segTree[0][2]);
+        }
+        return ans;
+    }
+};
