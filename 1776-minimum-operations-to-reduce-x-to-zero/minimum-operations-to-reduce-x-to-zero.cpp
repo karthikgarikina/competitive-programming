cc05cc2351d1f90a405b6488f35eef9319f5d897
karthikgarikina <155700123+karthikgarikina@users.noreply.github.com>
2026-09-23 13:12:23 +0530

    Time: 10 ms (29.84%) | Memory: 102.3 MB (91.82%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-        int n = nums.size();
-        int ans = INT_MAX;
-        int prefix_sum = 0;
-        unordered_map<int, int>Prefix;
+        int n = nums.size(), ans = INT_MAX, prefix_sum = 0, pre = -1;
         for(int i = 0; i < n; i++){
+            if(prefix_sum + nums[i] > x) break;
             prefix_sum += nums[i];
-            if(prefix_sum > x) break;
             if(prefix_sum == x) ans = i + 1;
-            Prefix[prefix_sum] = i + 1;
+            pre = i;
         }
         int suffix_sum = 0;
         for(int i = n - 1; i >= 0; i--){
             suffix_sum += nums[i];
-            if(suffix_sum > x) break;
-            if(suffix_sum == x) ans = min(ans, (n - i));
-            int rem = x - suffix_sum;
-            if(Prefix.count(rem) and Prefix[rem] - 1 < i) ans = min(ans, (n - i + Prefix[rem]));
+            while(pre >= 0 and ( pre >= i || (prefix_sum + suffix_sum) > x) ){
+                prefix_sum -= nums[pre--];
+            }
+            if(prefix_sum + suffix_sum == x) ans = min(ans, (n - i + pre + 1));
+            if(prefix_sum == 0 and suffix_sum > x) break;
         }
         if(ans == INT_MAX) return -1;
         return ans;
